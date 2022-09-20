932c7f5303dee02cc99dc05f7497a8253abb879f
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2022-09-20 13:56:19 +0700

    Update JK-ff.v

--- JK-ff.v
+++ JK-ff.v
@@ -11,11 +11,21 @@ wire S,R,S_g,R_g;
 				Qb = ~(R_g & Qa);
 endmodule
 
+//D-ff
+module D_ff(D,Clk,Qa,Qb);
+input D,Clk;
+output Qa,Qb;
+wire Cm,Qm;
+	assign	Cm = ~Clk;
+D_latch D1(D,Cm,Qm);
+D_latch D0(Qm,Clk,Qa,Qb);
+endmodule
+
 //JK-ff 
 module JK_ff(J,K,Clk,Q,Qn);
 input J,K,Clk;
 output Q,Qn;
 wire D;
 	assign D = (Qn & J) | (~K & Q);
-D_latch(D,Clk,Q,Qn);
+D_ff(D,Clk,Q,Qn);
 endmodule
